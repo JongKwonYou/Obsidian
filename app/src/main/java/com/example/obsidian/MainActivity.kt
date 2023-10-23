7651549e826b555675fd4191041ab4d4242b0bfa
JongKwonYou <yjk.dev.9866@healthport.co.kr>
2023-10-23 09:44:02 +0900

    feat - temp

--- app/src/main/java/com/example/obsidian/MainActivity.kt
+++ app/src/main/java/com/example/obsidian/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.obsidian.ui.theme.ObsidianTheme
 
+// todo - compose preview
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
